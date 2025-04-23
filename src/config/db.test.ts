4005c57c8227a4b45ef6708c5105a0910d6ad195
Vinicius <viniciusep181@gmail.com>
2025-04-23 12:55:08 -0300

    test: db test

---
+++ src/config/db.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it, vi } from 'vitest'
+import { connectDB } from './db'
+
+vi.mock("mongoose", { spy: true })
+
+describe(connectDB.name, () => {
+  it("should throw error if MONGO_DB is not gived", () => {
+    expect(connectDB()).rejects.toThrowError()
+  })
+
+  //TODO: make this test work
+ /*  it("should be sucess if MONGO_DB is gived", async () => {
+    vi.stubEnv("MONGO_DB", "98012hbasdibaasdadaskdhjgb")
+    const x = await import("./db")
+
+    expect(x.connectDB()).rejects.not.toThrowError()
+  }) */
+})
